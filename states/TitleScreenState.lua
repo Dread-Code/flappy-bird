afbae468fd4fcf0505ac864acdc263d17dbfcc02
Dread-Code <lucasospina97@gmail.com>
2022-11-14 21:54:29 -0500

    BaseState and states created

---
+++ states/TitleScreenState.lua
@@ -0,0 +1,30 @@
+--[[
+    TitleScreenState Class
+    
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    The TitleScreenState is the starting screen of the game, shown on startup. It should
+    display "Press Enter" and also our highest score.
+]]
+
+--[[
+    includes = extends in TS
+    Basically with the class macro we could make
+    inheritance
+]]
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('play')
+    end
+end
+
+function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Flappy Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+end
